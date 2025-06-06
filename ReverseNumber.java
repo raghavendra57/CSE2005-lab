4922bf80922cea4d17fb860ccd7f51812b24ce55
raghavendra57 <kjsraghavendra2005@gmail.com>
2025-06-06 16:42:23 +0530

    Lab-1

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int number = 12345;
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reversed number: " + reversed);
+    }
+}
